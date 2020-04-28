cbe6841a8db23b3a775db2bad547e060c5f6b655
Jelena <jelena.mikan95@gmail.com>
2020-04-28 23:37:31 +0200

    lab 2

---
+++ Assets/EndGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > 120)
+        {
+            Debug.Log("Quit");
+            SceneManager.LoadScene("EndMenu");
+        } 
+    }
+}
---
+++ Assets/GvrButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class GvrButton : MonoBehaviour
+{
+    public Image imgCircle;
+    public UnityEvent GvrClick;
+    public float totalTime = 2;
+    bool gvrStatus;
+    public float gvrTimer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Invoked");
+        if (gvrStatus)
+        {
+            gvrTimer += Time.deltaTime;
+            Debug.Log(gvrTimer);
+            imgCircle.fillAmount = gvrTimer / totalTime;
+        }
+
+        if(gvrTimer > totalTime)
+        {
+            Debug.Log("WTF");
+            GvrClick.Invoke();
+            imgCircle.fillAmount = 0;
+            gvrTimer = 0;
+        }
+    }
+
+    public void GvrOn()
+    {
+        gvrStatus = true;
+    }
+
+    public void GvrOff()
+    {
+        gvrStatus = false;
+        gvrTimer = 0;
+        imgCircle.fillAmount = 0;
+    }
+}
