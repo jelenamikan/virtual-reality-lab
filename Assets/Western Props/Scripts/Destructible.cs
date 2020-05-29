5483cff8af1df4cff02092bf4f54e290e4411bee
Jelena <jelena.mikan95@gmail.com>
2020-05-29 22:24:44 +0200

    lab4

--- Assets/Western Props/Scripts/Destructible.cs
+++ Assets/Western Props/Scripts/Destructible.cs
@@ -13,16 +13,31 @@ public class Destructible : MonoBehaviour {
 	public GameObject destroyedVersion;	// Reference to the shattered version of the object
     public GameObject candle1;
     public GameObject candle2;
+    public AudioSource src;
+    public AudioClip clip;
+
+    void Start()
+    {
+        src = GetComponent<AudioSource>();
+    }
 
 	// If the player clicks on the object
 	void OnMouseDown ()
 	{
 		// Spawn a shattered object
 		Instantiate(destroyedVersion, transform.position, transform.rotation);
-		// Remove the current object
-		Destroy(gameObject);
+        src.PlayOneShot(clip, 0.8F);
+        StartCoroutine(Wait());
+        // Remove the current object
+        Destroy(gameObject);
         candle1.GetComponent<Collider>().enabled = true;
         candle2.GetComponent<Collider>().enabled = true;
+    }
+
+    IEnumerator Wait()
+    {
+        //yield on a new YieldInstruction that waits for 1 second.
+        yield return new WaitForSeconds(1);
     }
 
 }
